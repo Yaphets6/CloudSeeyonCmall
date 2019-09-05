9cf51bbc8d63ec3e25e745f40a235f7403af7254
Yaphets <664785288@qq.com>
2019-09-05 23:06:50 +0800

    删除无效注释

--- src/test_case/mysql_test1.py
+++ src/test_case/mysql_test1.py
@@ -4,7 +4,7 @@ from src.case_center.excel_case_handle import CaseSheet
 case_model = CaseModel()
 case_model.generate_excel_case_file_model()
 case_model.initialization_case_info_table()
-case = CaseSheet("F:\\Study\\CloudSeeyonCmall\\src\\case_files\\demo2.xlsx")
+case = CaseSheet("F:\\Study\\CloudSeeyonCmall\\src\\case_files\\用例模板.xlsx")
 content = case.get_all_case_content()
 print(content[0])
 print(content[1])
